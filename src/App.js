201d98fb197cf6928f56785a7cf24031dacbb74d
Qutadah95 <qutadaalmomani@yahoo.com>
2021-09-12 17:15:29 +0300

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+// import Login from './Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
@@ -37,8 +38,10 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/">
               {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-            </Route>
+            </Route >
+            <Route exact path="./Profile">
             {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+            </Route >
           </Switch>
           <Footer />
         </Router>
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,11 +1,41 @@
 import { Component } from "react";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 class LoginForm extends Component {
-
+constructor(props){
+  super(props);
+  this.state={
+    
+  }
+  
+}
   render() {
     /* TODO: create a simple login form that collects username and and email, and lets parent component know when form has been submitted */
+ 
     return (
-      <p>LoginForm Coming Soon</p>
+      <div>
+           <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+      </div>
     );
   }
 };
