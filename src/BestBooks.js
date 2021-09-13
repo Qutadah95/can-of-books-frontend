210f597ea660317ac78d650d7f67c0f13b01a824
NashatAlzaatreh <nashat.alzaatra@yahoo.com>
2021-09-14 00:13:30 +0300

    editing lab12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import AddBook from "./AddBook";
+import "./index.css";
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -22,60 +23,39 @@ class BestBooks extends React.Component {
       status: e.target.bookStatus.value,
       email: e.target.email.value,
     };
-    console.log("******************************");
-    console.log(reqBody);
-    console.log(e.target.bookTitle.value);
-    console.log("******************************");
-
-    /**
-     * Using axios you want to send the data back to the backend so it would create a new cat
-     *
-     * After that when you get the response for the added new cat, update the state with the new cat
-     */
 
     axios
       .post(`${process.env.REACT_APP_API_URL}/books`, reqBody)
       .then((createdBookObject) => {
-        this.state.booksData.push(createdBookObject.data); // push the new data into the state of the catsData
-        this.setState({ booksData: this.state.booksData }); // update the data using setState to invoke the re-render
-        this.handelDisplayAddModal(); // close the modal after we are done!
+        this.state.booksData.push(createdBookObject.data);
+        this.setState({ booksData: this.state.booksData });
+        this.handelDisplayAddModal();
       })
       .catch(() => alert("Something went wrong while posting!"));
   };
 
-  /**
-   * Handel the Deletion of a cat by its ID
-   */
   handelDeleteBook = (bookId) => {
-    /**
-     * Using axios you want to send the request with the ID of the cat as a param to backend so it delete that cat
-     *
-     * After that when you get the response and check if the delete count is == 1
-     * if its == 1 then remove that cat from the state and the set the state to invoke the render function again.
-     */
-
-    // console.log('cat ID', catId);
-
+    console.log("1");
     console.log(`${process.env.REACT_APP_API_URL}/books/${bookId}`);
 
     axios
       .delete(`${process.env.REACT_APP_API_URL}/books/${bookId}`)
       .then((deleteResponse) => {
+        console.log("2");
+        console.log(`${process.env.REACT_APP_API_URL}/books`);
+        console.log(deleteResponse.data.deletedCount === 1);
         if (deleteResponse.data.deletedCount === 1) {
-          const newBookArr = this.state.catsData.filter(
+          const newBookArr = this.state.booksData.filter(
             (book) => book._id !== bookId
           );
-
-          /**
-           * I want to filter out the cat ID that I deleted, by returning only the cat object that doesn't match the id of the
-           * cat that I deleted
-           */
+          console.log("3");
+          console.log(`${process.env.REACT_APP_API_URL}/books`);
           this.setState({ booksData: newBookArr });
         }
       })
       .catch(() => alert("something went wrong while deleting"));
-
-    console.log(`${process.env.REACT_APP_API_URL}/books/${bookId}`);
+    console.log("4");
+    console.log(`${process.env.REACT_APP_API_URL}/books`);
   };
 
   handelDisplayAddModal = () => {
@@ -89,7 +69,6 @@ class BestBooks extends React.Component {
         this.setState({ booksData: bookResponse.data });
       })
       .catch(() => alert("the book collection is empty."));
-    console.log(`${process.env.REACT_APP_API_URL}/books`);
   };
   render() {
     return (
@@ -104,8 +83,6 @@ class BestBooks extends React.Component {
             {this.state.booksData.map((book) => {
               return (
                 <div>
-                  {/* ================================================ */}
-                  {/* Show/ Hide the Add New Cat Modal Form */}
                   {this.state.showAddModal && (
                     <>
                       <AddBook
@@ -115,10 +92,9 @@ class BestBooks extends React.Component {
                       />
                     </>
                   )}
-                  {/* ================================================ */}
-                  <>
-                    <Card style={{ width: "18rem" }}>
-                      {/* <Card.Img variant="top" src={books.img} /> */}
+
+                  <div id="booksDiv">
+                    <Card>
                       <Card.Body>
                         <Card.Title>{book.title}</Card.Title>
                         <Card.Text>{book.description}</Card.Text>
@@ -132,7 +108,7 @@ class BestBooks extends React.Component {
                         </Button>
                       </Card.Body>
                     </Card>
-                  </>
+                  </div>
                 </div>
               );
             })}
