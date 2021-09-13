c08705e18cac63217eb1d894960aa0728ef0d769
NashatAlzaatreh <nashat.alzaatra@yahoo.com>
2021-09-13 17:04:38 +0300

    lab12 frontEnd Done

--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import LoginButton from './LoginButton';
+import React from "react";
+import Card from "react-bootstrap/Card";
+// import Button from 'react-bootstrap/Button';
+// import LoginButton from './LoginButton';
 // import './login.css';
 
 class Login extends React.Component {
   render() {
     return (
-      <Card style={{ width: '18rem' }}>
+      <Card style={{ width: "18rem" }}>
         <Card.Body>
           <Card.Title>Log In</Card.Title>
-          <Card.Text>
-            Click Below to Log In
-          </Card.Text>
+          <Card.Text>Click Below to Log In</Card.Text>
           {/* TODO: add a `LoginButton` component here that will log the user in */}
           {/* <Button variant="primary" onClick={LoginButton}>LoginButton</Button> */}
-          <a href='./LoginButton'>LoginButton</a>
+          <a href="./LoginButton">LoginButton</a>
         </Card.Body>
       </Card>
-    )
+    );
   }
 }
 
